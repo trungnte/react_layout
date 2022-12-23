577481b3965a25b6909421e6dba6598bbe4e4860
Trung <trung.nt.e@gmail.com>
2022-12-23 16:09:11 +0700

    Footer

--- src/BaiTapLayoutComponent/LayoutComponent.jsx
+++ src/BaiTapLayoutComponent/LayoutComponent.jsx
@@ -40,6 +40,11 @@ export default class LayoutComponent extends Component {
           </div>
         </div>
         
+        <footer className='py-5 bg-dark'>
+          <div className='container'>
+            <p className='m-0 text-center text-white'>Copyright © Your Website 2022</p>
+          </div>
+        </footer>
 
       </div>
     )
